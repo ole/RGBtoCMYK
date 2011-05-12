b4a5e32c3a3161e33f2e0faf91ed6c9581be9599
Ole Begemann <ole@oleb.net>
2011-05-12 21:41:06 +0200

    Add code to convert every image passed on the command line to CMYK

--- RGBtoCMYK/main.m
+++ RGBtoCMYK/main.m
@@ -2,21 +2,59 @@
 //  main.m
 //  RGBtoCMYK
 //
-//  Created by Ole Begemann on 12.05.11.
-//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//  Created by Ole Begemann in May 2011.
+//  Copyright 2011 Ole Begemann.
+//  License: MIT License
 //
 
-#import <Foundation/Foundation.h>
+#import <Cocoa/Cocoa.h>
+
+NSBitmapImageRep *convertImageFileToCMYK(NSString *sourceFilename)
+{
+    NSImage *sourceImage = [[[NSImage alloc] initWithContentsOfFile:sourceFilename] autorelease];
+    NSBitmapImageRep *sourceImageRep = [[sourceImage representations] objectAtIndex:0];
+    
+    NSColorSpace *targetColorSpace = [NSColorSpace genericCMYKColorSpace];
+    NSBitmapImageRep *targetImageRep = [sourceImageRep bitmapImageRepByConvertingToColorSpace:targetColorSpace 
+        renderingIntent: NSColorRenderingIntentPerceptual];
+    return targetImageRep;
+}
+
+
+BOOL writeBitmapImageRepToFile(NSBitmapImageRep *imageRep, NSString *targetFilename)
+{
+    NSData *tiffData = [imageRep TIFFRepresentation];
+    return [tiffData writeToFile:targetFilename atomically:NO];
+}
+
 
 int main (int argc, const char * argv[])
 {
+    NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
 
-    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    NSMutableArray *commandLineArguments = [NSMutableArray arrayWithArray:[[NSProcessInfo processInfo] arguments]];
+    [commandLineArguments removeObjectAtIndex:0]; // first object is the name of the executable
+    
+    [commandLineArguments enumerateObjectsWithOptions:NSEnumerationConcurrent usingBlock:^(id filename, NSUInteger idx, BOOL *stop) {
+        NSAutoreleasePool *loopPool = [[NSAutoreleasePool alloc] init];
 
-    // insert code here...
-    NSLog(@"Hello, World!");
-
+        NSBitmapImageRep *cmykImageRep = convertImageFileToCMYK(filename);
+        BOOL success = NO;
+        if (cmykImageRep != nil) {
+            NSString *baseFilename = [filename stringByDeletingPathExtension];
+            NSString *targetFilename = [[baseFilename stringByAppendingString:@"-CMYK"] stringByAppendingPathExtension:@"tiff"];
+            success = writeBitmapImageRepToFile(cmykImageRep, targetFilename);
+        }
+        
+        if (success) {
+            fprintf(stdout, "Success: %s\n", [filename UTF8String]);
+        } else {
+            fprintf(stderr, "Failure: %s\n", [filename UTF8String]);
+        }
+        
+        [loopPool drain];
+    }];
+    
     [pool drain];
     return 0;
 }
-
