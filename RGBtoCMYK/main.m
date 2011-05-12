e88af7469a7ab82e7df9701db7985c687c0e0e8a
Ole Begemann <ole@oleb.net>
2011-05-12 20:52:26 +0200

    Initial Commit

---
+++ RGBtoCMYK/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  RGBtoCMYK
+//
+//  Created by Ole Begemann on 12.05.11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[])
+{
+
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+
+    // insert code here...
+    NSLog(@"Hello, World!");
+
+    [pool drain];
+    return 0;
+}
+
